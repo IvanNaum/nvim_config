bddf9192801c43ba3588a46aea50248bcd46ff7e
IvanNaum <nuk04@mail.ru>
2023-09-13 17:55:47 +0300

    Refactored

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,19 +10,19 @@ return require('packer').startup(function(use)
   -- Информационная строка внизу
   use "kyazdani42/nvim-web-devicons"
   use { 'nvim-lualine/lualine.nvim',
-      requires = {'kyazdani42/nvim-web-devicons', opt = true},
-      config = function()
-        require('lualine').setup()
-      end,
-    }
-  -- Тема 
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    config = function()
+      require('lualine').setup()
+    end,
+  }
+  -- Тема
   use({
     'NLKNguyen/papercolor-theme',
     as = 'papercolor-theme',
     config = function()
-    vim.cmd('set t_Co=256')
-    vim.cmd('set background=dark')
-    vim.cmd('colorscheme PaperColor')
+      vim.cmd('set t_Co=256')
+      vim.cmd('set background=dark')
+      vim.cmd('colorscheme PaperColor')
     end
   })
 
@@ -32,14 +32,14 @@ return require('packer').startup(function(use)
   ---------------------------------------------------------
   -- автоматические закрывающиеся скобки
   use { 'windwp/nvim-autopairs',
-      config = function()
-        require("nvim-autopairs").setup()
-      end,
+    config = function()
+      require("nvim-autopairs").setup()
+    end,
   }
   use { 'numToStr/Comment.nvim',
-      config = function()
-        require('Comment').setup()
-      end
+    config = function()
+      require('Comment').setup()
+    end
   }
 
   ---------------------------------------------------------
@@ -47,9 +47,9 @@ return require('packer').startup(function(use)
   ---------------------------------------------------------
   -- LSP
   use {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
   }
   -- Autocomplete
   use {
@@ -68,13 +68,16 @@ return require('packer').startup(function(use)
     -- end,
     requires = { "nvim-lua/plenary.nvim" },
   })
-  use {"jay-babu/mason-null-ls.nvim"}
-
+  use { "jay-babu/mason-null-ls.nvim" }
+  -- DAP
+  use {
+    "rcarriga/nvim-dap-ui",
+    requires = { "mfussenegger/nvim-dap" }
+  }
 
   ---------------------------------------------------------
   -- РАЗНОЕ
   ---------------------------------------------------------
   -- Даже если включена русская раскладка, то nvim-команды будут работать
   use 'powerman/vim-plugin-ruscmd'
-
 end)
